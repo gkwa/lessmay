ab16ed217b07423f1e9dec2087f4a52231c45414
Taylor Monacelli <taylormonacelli@gmail.com>
2024-08-20 18:18:41 -0700

    move logic out of cmd and into core

--- cmd/showConflicts.go
+++ cmd/showConflicts.go
@@ -1,11 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
-
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 
 	"github.com/gkwa/lessmay/core"
@@ -22,24 +17,10 @@ var showConflictsCmd = &cobra.Command{
 		logger := LoggerFrom(cmd.Context())
 		logger.Info("Running showConflicts command")
 
-		var paths []string
-		if len(args) == 0 {
-			paths = append(paths, defaultObsidianPath)
-		} else {
-			for _, arg := range args {
-				expandedPath, err := homedir.Expand(arg)
-				if err != nil {
-					logger.Error(err, "Failed to expand path", "path", arg)
-					os.Exit(1)
-				}
-				paths = append(paths, expandedPath)
-			}
-		}
-
-		resolver := core.NewSyncConflictResolver(logger)
-		if err := resolver.ResolveSyncConflicts(paths, skipPaths); err != nil {
+		if err := core.ShowConflicts(logger, args, defaultObsidianPath, skipPaths); err != nil {
 			logger.Error(err, "Failed to resolve sync conflicts")
-			os.Exit(1)
+			cmd.PrintErrln("Error:", err)
+			cmd.PrintErrln("Run with --verbose for more details.")
 		}
 	},
 }
@@ -47,12 +28,7 @@ var showConflictsCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(showConflictsCmd)
 
-	home, err := homedir.Dir()
-	if err != nil {
-		fmt.Println("Error getting home directory:", err)
-		os.Exit(1)
-	}
-	defaultObsidianPath = filepath.Join(home, "Documents", "Obsidian Vault")
+	defaultObsidianPath = core.GetDefaultObsidianPath()
 
 	showConflictsCmd.Flags().StringVarP(&defaultObsidianPath, "default-path", "d", defaultObsidianPath, "Default Obsidian vault path")
 }
--- core/core.go
+++ core/core.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 
 	"github.com/go-logr/logr"
+	homedir "github.com/mitchellh/go-homedir"
 )
 
 type FileFinder interface {
@@ -149,4 +150,39 @@ func (c *DefaultFileComparer) CompareAndDelete(conflictFile, originalFile string
 	}
 
 	return false, nil
+}
+
+func ShowConflicts(logger logr.Logger, args []string, defaultObsidianPath string, skipPaths []string) error {
+	paths, err := getConflictPaths(args, defaultObsidianPath)
+	if err != nil {
+		return fmt.Errorf("failed to get conflict paths: %w", err)
+	}
+
+	resolver := NewSyncConflictResolver(logger)
+	return resolver.ResolveSyncConflicts(paths, skipPaths)
+}
+
+func getConflictPaths(args []string, defaultObsidianPath string) ([]string, error) {
+	var paths []string
+	if len(args) == 0 {
+		paths = append(paths, defaultObsidianPath)
+	} else {
+		for _, arg := range args {
+			expandedPath, err := homedir.Expand(arg)
+			if err != nil {
+				return nil, fmt.Errorf("failed to expand path %s: %w", arg, err)
+			}
+			paths = append(paths, expandedPath)
+		}
+	}
+	return paths, nil
+}
+
+func GetDefaultObsidianPath() string {
+	home, err := homedir.Dir()
+	if err != nil {
+		fmt.Println("Error getting home directory:", err)
+		os.Exit(1)
+	}
+	return filepath.Join(home, "Documents", "Obsidian Vault")
 }
